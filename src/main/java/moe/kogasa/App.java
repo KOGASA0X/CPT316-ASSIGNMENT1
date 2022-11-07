e89736448a05ee0c400be22122154fc65c9ad4d2
KOGASA0X <misaka17447@gmail.com>
2022-11-08 00:28:10 +0800

    feat: string sort function completed

--- src/main/java/moe/kogasa/App.java
+++ src/main/java/moe/kogasa/App.java
@@ -10,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        Logic logic=new Logic();
+        String str="1589404u659294437689";
+        System.out.println(logic.insertion(str));
         //System.out.println( "Hello World!" );
         JFrame frame = new JFrame("CPT316");
         frame.setSize(350, 200);
@@ -18,28 +21,6 @@ public class App
         JPanel panel=new JPanel();
         frame.add(panel);
         frame.setVisible(true);
-    }
-
-    String bubble(String str)
-    {
-        StringBuilder temp=new StringBuilder(str);
-        for (int i = 0; i < str.length()-1; i++) {
-            if (temp.charAt(i)>(int)temp.charAt(i+1)) {
-                char c= temp.charAt(i);
-                temp.setCharAt(i, temp.charAt(i+1));
-                temp.setCharAt(i+1, c);
-            }
-        }
-        return temp.toString();
-    }
-    String selection(String str)
-    {
-
-        return null;
-    }
-    String insertion(String str)
-    {
-        return null;
     }
 
 }
---
+++ src/main/java/moe/kogasa/Logic.java
@@ -0,0 +1,85 @@
+package moe.kogasa;
+public class Logic {
+    
+    String bubble(String str)
+    {
+        StringBuilder temp=new StringBuilder(str);
+        boolean flag=true;
+        while (flag) {
+            flag =false;
+            for (int i = 0; i < str.length()-1; i++) {
+                if (temp.charAt(i)>(int)temp.charAt(i+1)) {
+                    char c= temp.charAt(i);
+                    temp.setCharAt(i, temp.charAt(i+1));
+                    temp.setCharAt(i+1, c);
+                    flag=true;
+                }
+            }
+        }
+        
+        return temp.toString();
+    }
+
+
+    String selection(String str)
+    {
+        StringBuilder order=new StringBuilder();
+        StringBuilder disoder=new StringBuilder(str);
+
+        while(order.length()!=str.length())
+        {
+            char temp=disoder.charAt(0);
+            int loc=0;
+            for (int i = 0; i < disoder.length(); i++) {
+                if(disoder.charAt(i)<temp)
+                {
+                    temp=disoder.charAt(i);
+                    loc=i;
+                }
+            }
+            disoder.deleteCharAt(loc);
+            order.append(temp);
+        }
+
+        return order.toString();
+    }
+
+    String insertion(String str)
+    {
+        StringBuilder s1=new StringBuilder(str);
+        for (int i = 1; i < s1.length(); i++)
+        {
+            boolean flag = true;
+            int loc=i;
+            while(flag)
+            {
+                
+                if(s1.charAt(loc)<s1.charAt(loc-1))
+                {
+                    char temp=s1.charAt(loc);
+                    s1.setCharAt(loc, s1.charAt(loc-1));
+                    s1.setCharAt(loc-1, temp);
+
+                    if (loc-1>0) {
+                        loc--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    
+                }
+                else
+                {
+                    flag =false;
+                }
+                
+            }
+                
+
+            
+        }
+        return s1.toString();
+    }
+
+}
