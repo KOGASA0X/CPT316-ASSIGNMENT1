1471d1c2f896c3e1c6906249fbd4754bb8462b64
KOGASA0X <misaka17447@gmail.com>
2022-11-07 19:38:01 +0800

    feat: GUI & Bubble

--- src/main/java/moe/kogasa/App.java
+++ src/main/java/moe/kogasa/App.java
@@ -1,13 +1,46 @@
 package moe.kogasa;
-
+import javax.swing.*;
 /**
  * Hello world!
  *
  */
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+        JFrame frame = new JFrame("CPT316");
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel=new JPanel();
+        frame.add(panel);
+        frame.setVisible(true);
     }
+
+    String bubble(String str)
+    {
+        StringBuilder temp=new StringBuilder(str);
+        for (int i = 0; i < str.length()-1; i++) {
+            if (temp.charAt(i)>(int)temp.charAt(i+1)) {
+                char c= temp.charAt(i);
+                temp.setCharAt(i, temp.charAt(i+1));
+                temp.setCharAt(i+1, c);
+            }
+        }
+        return temp.toString();
+    }
+    String selection(String str)
+    {
+
+        return null;
+    }
+    String insertion(String str)
+    {
+        return null;
+    }
+
 }
+
